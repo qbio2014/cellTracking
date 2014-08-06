7c65cd85ea75ddc2a154aaf033cf080f78f51109
Richard Neher <richard@ag-neher-xps13.(none)>
2014-08-06 16:40:39 -0700

    images are now loaded from file on demand

--- src/sahand_test.py
+++ src/sahand_test.py
@@ -1,10 +1,10 @@
-from segmentation_tools import *
+import segmentation_tools as st
 from Bio import Phylo
 import matplotlib.pyplot as plt
 import os
 plt.ion()
 
-myS = labeled_series()
+myS = st.labeled_series()
 myS.load_from_file('../sahand_test/pred/Yutao1-17_1-c-???.h5_processed.h5', '../sahand_test/Yutao1-17_1-c-???.h5', p_cutoff=0.7) 
 plt.figure()
 plt.hist(myS.get_object_props_allTimes('area'), bins=100)
@@ -16,7 +16,7 @@ upper_th = int(raw_input('Enter upper cut_off: '))
 #myS.filter_objects('area', lower_th = lower_th, upper_th = upper_th)
 
 myS.filter_objects_multiProp({'area':(lower_th,upper_th)},'OR')
-myS.track_objects(match_func=match_mindist)
+myS.track_objects(match_func=st.match_mindist)
 
 myS.color_lineages()
 qc_dir = '../sahand_test/QC'
